cae8322f00b115ef69d31bb3b49ebb053e5905d9
Sergio Pereira <72481029+sergio1pj@users.noreply.github.com>
2022-07-17 00:13:33 -0300

    corrigindo bugs

--- anagrams1/anagrams1.js
+++ anagrams1/anagrams1.js
@@ -1,21 +1,21 @@
 const button = document.getElementById("findButton");
+const anagramsList = document.createElement("ul");
 function alphabetize(a) {
     return a.toLowerCase().split("").sort().join("").trim();
 }
 function getAnagramsOf(input) {
     const alphabeticalInput = alphabetize(input);
     const anagrams = palavras.filter(palavra => alphabetize(palavra) === alphabeticalInput);
-    let anagramsList = document.createElement("ul");
+    anagramsList.innerHTML = "";
     anagrams.forEach(anagram => {
         let tagLi = document.createElement("li");
         tagLi.innerText = anagram;
         anagramsList.appendChild(tagLi);
-        return false;
     })
-    document.body.appendChild(anagramsList)
     return anagrams;
 }
 button.addEventListener("click", function () {
     let typedText = document.getElementById("input").value;
     getAnagramsOf(typedText);
 });
+document.body.appendChild(anagramsList)
